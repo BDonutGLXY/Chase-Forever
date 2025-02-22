f9643113572cd06525ba93d1378ca71392d5e5c0
BetterDonutGalaxy <134549886+BDonutGLXY@users.noreply.github.com>
2025-02-22 10:55:40 -0600

    Add files via upload

---
+++ Individuals/DL_UFOGameColors.lua
@@ -0,0 +1,162 @@
+freeslot("SKINCOLOR_BELL",
+"SKINCOLOR_CARBON",
+"SKINCOLOR_CAMPANELLA",
+"SKINCOLOR_UFOCHERRY",
+"SKINCOLOR_WAXING",
+"SKINCOLOR_LARD",
+"SKINCOLOR_GIBBOUS",
+"SKINCOLOR_EGG",
+"SKINCOLOR_HIGHSCORE",
+"SKINCOLOR_WIREFRAME",
+"SKINCOLOR_SEASIDE",
+"SKINCOLOR_CART",
+"SKINCOLOR_COAT",
+"SKINCOLOR_SAUCER",
+"SKINCOLOR_WANING",
+"SKINCOLOR_NOSTALGIA"
+)
+
+skincolors[SKINCOLOR_BELL] = {
+name = "Bell",
+ramp = {0,0,0,0,6,6,6,6,17,17,17,17,31,31,31,31},
+invcolor = SKINCOLOR_CARBON,
+invshade = 7,
+chatcolor = V_REDMAP,
+accessible = true
+}
+
+skincolors[SKINCOLOR_CARBON] = {
+name = "Carbon",
+ramp = {6,6,6,6,17,17,17,17,31,31,31,31,31,31,31,31},
+invcolor = SKINCOLOR_BELL,
+invshade = 7,
+chatcolor = V_REDMAP,
+accessible = true
+}
+
+skincolors[SKINCOLOR_CAMPANELLA] = {
+name = "Campanella",
+ramp = {0,0,0,0,33,33,33,33,207,207,207,207,31,31,31,31},
+invcolor = SKINCOLOR_COAT,
+invshade = 7,
+chatcolor = V_REDMAP,
+accessible = true
+}
+-- I forgot I had a skincolor in my main skincolor mod called Cherry so uh fuck
+skincolors[SKINCOLOR_UFOCHERRY] = {
+name = "Cherry",
+ramp = {33,33,33,33,37,37,37,37,207,207,207,207,31,31,31,31},
+invcolor = SKINCOLOR_CART,
+invshade = 7,
+chatcolor = V_REDMAP,
+accessible = true
+}
+
+skincolors[SKINCOLOR_WAXING] = {
+name = "Waxing",
+ramp = {0,0,0,0,32,32,32,32,102,102,102,102,31,31,31,31},
+invcolor = SKINCOLOR_WANING,
+invshade = 7,
+chatcolor = V_BLUEMAP,
+accessible = true
+}
+
+skincolors[SKINCOLOR_LARD] = {
+name = "Lard",
+ramp = {0,0,0,0,50,50,50,50,58,58,58,58,31,31,31,31},
+invcolor = SKINCOLOR_COAT,
+invshade = 7,
+chatcolor = V_ORANGEMAP,
+accessible = true
+}
+
+skincolors[SKINCOLOR_GIBBOUS] = {
+name = "Gibbous",
+ramp = {0,0,0,0,50,50,50,50,122,122,122,122,31,31,31,31},
+invcolor = SKINCOLOR_WANING,
+invshade = 7,
+chatcolor = V_ORANGEMAP,
+accessible = true
+}
+
+skincolors[SKINCOLOR_EGG] = {
+name = "Egg",
+ramp = {0,0,0,0,64,64,64,64,76,76,76,76,31,31,31,31},
+invcolor = SKINCOLOR_NOSTALGIA,
+invshade = 7,
+chatcolor = V_YELLOWMAP,
+accessible = true
+}
+
+skincolors[SKINCOLOR_HIGHSCORE] = {
+name = "High Score",
+ramp = {0,0,0,0,64,64,64,64,54,54,54,54,31,31,31,31},
+invcolor = SKINCOLOR_NOSTALGIA,
+invshade = 7,
+chatcolor = V_YELLOWMAP,
+accessible = true
+}
+
+skincolors[SKINCOLOR_WIREFRAME] = {
+name = "Wireframe",
+ramp = {0,0,0,0,121,121,121,121,142,142,142,142,31,31,31,31},
+invcolor = SKINCOLOR_CAMPANELLA,
+invshade = 7,
+chatcolor = V_CYANMAP,
+accessible = true
+}
+
+skincolors[SKINCOLOR_SEASIDE] = {
+name = "Seaside",
+ramp = {0,0,0,0,121,121,121,121,114,114,114,114,31,31,31,31},
+invcolor = SKINCOLOR_CAMPANELLA,
+invshade = 7,
+chatcolor = V_CYANMAP,
+accessible = true
+}
+
+skincolors[SKINCOLOR_CART] = {
+name = "Cart",
+ramp = {121,121,121,121,141,141,141,141,136,136,136,136,31,31,31,31},
+invcolor = SKINCOLOR_UFOCHERRY,
+invshade = 7,
+chatcolor = V_BLUEMAP,
+accessible = true
+}
+
+skincolors[SKINCOLOR_COAT] = {
+name = "Even Coat",
+ramp = {0,0,0,0,133,133,133,133,136,136,136,136,31,31,31,31},
+invcolor = SKINCOLOR_CAMPANELLA,
+invshade = 7,
+chatcolor = V_BLUEMAP,
+accessible = true
+}
+
+skincolors[SKINCOLOR_SAUCER] = {
+name = "Saucer",
+ramp = {0,0,0,0,242,242,242,242,137,137,137,137,31,31,31,31},
+invcolor = SKINCOLOR_UFOCHERRY,
+invshade = 7,
+chatcolor = V_BLUEMAP,
+accessible = true
+}
+
+skincolors[SKINCOLOR_WANING] = {
+name = "Waning",
+ramp = {0,0,0,0,133,133,133,133,34,34,34,34,31,31,31,31},
+invcolor = SKINCOLOR_WAXING,
+invshade = 7,
+chatcolor = V_BLUEMAP,
+accessible = true
+}
+
+skincolors[SKINCOLOR_NOSTALGIA] = {
+name = "Nostalgia",
+ramp = {0,0,0,0,181,181,181,181,164,164,164,164,31,31,31,31},
+invcolor = SKINCOLOR_HIGHSCORE,
+invshade = 7,
+chatcolor = V_REDMAP,
+accessible = true
+}
+
---
+++ Individuals/DL_UFOMiscColors.lua
@@ -0,0 +1,20 @@
+freeslot("SKINCOLOR_MOONFLOWER",
+"SKINCOLOR_REBIRTH")
+
+skincolors[SKINCOLOR_MOONFLOWER] = {
+name = "Moonflower",
+ramp = {0,144,145,146,147,134,134,195,37,39,41,43,45,46,47,31},
+invcolor = SKINCOLOR_REBIRTH,
+invshade = 7,
+chatcolor = V_BLUEMAP,
+accessible = true
+}
+
+skincolors[SKINCOLOR_REBIRTH] = {
+name = "Rebirth",
+ramp = {0,208,48,216,49,50,85,99,123,124,125,126,127,139,139,254},
+invcolor = SKINCOLOR_MOONFLOWER,
+invshade = 7,
+chatcolor = V_ORANGEMAP,
+accessible = true
+}
